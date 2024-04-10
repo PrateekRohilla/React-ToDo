0adb462249107439deca45e06662c3a96cc88ba6
Prateek Rohilla <prateek98rohilla@gmail.com>
2024-04-10 15:52:56 +0530

    Todo layout

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,27 @@ function App() {
   return (
     <>
       <Navbar/>
-      Your ToDos
+      <div className='container mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh]'>
+        <div className="addTodo my-5">
+          <h2 className='text-lg font-bold'>Add a To Do</h2>
+          <input type="text" className='w-1/2'/>
+          <button className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-6'>
+            Add
+          </button>
+        </div>
+        <h2 className='text-lg font-bold'>Your Todos</h2>
+        <div className="todos">
+          <div className="todo flex">
+            <div className="text">
+              Lorem ipsum dolor sit amet.
+            </div>
+            <div className="button">
+              <button onClick={handleEdit} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1'>Edit</button>
+              <button onClick={handleDelete} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1'>Delete</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-        Hey I am a navbar
-    </div>
+    <nav className='flex justify-between bg-indigo-800 text-white py-2'>
+      <div className='logo'>
+        <span className='font-bold text-xl mx-8'>To-Do</span>
+      </div>
+      <ul className='flex gap-8 mx-9'>
+        <li className='cursor-pointer hover:font-bold transition-all'>Home</li>
+        <li className='cursor-pointer hover:font-bold transition-all'>Your Tasks</li>
+      </ul>
+    </nav>
   )
 }
 
